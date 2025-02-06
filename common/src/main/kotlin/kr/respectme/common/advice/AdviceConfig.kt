a8d3ba82ce274401bccdbe7258c9f6b76cbe6c44
dohoon <elensar92@gmail.com>
2025-02-06 22:50:58 +0900

    file api 및 common config 수정

--- common/src/main/kotlin/kr/respectme/common/advice/AdviceConfig.kt
+++ common/src/main/kotlin/kr/respectme/common/advice/AdviceConfig.kt
@@ -1,4 +1,27 @@
 package kr.respectme.common.advice
 
-class AdviceConfig {
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.springframework.boot.autoconfigure.AutoConfiguration
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+
+@Configuration
+@AutoConfiguration
+class AdviceConfig(private val objectMapper: ObjectMapper) {
+
+    @Bean
+    fun generalExceptionHandler(): GeneralExceptionHandlerAdvice {
+        return GeneralExceptionHandlerAdvice(objectMapper)
+    }
+
+    @Bean
+    fun envelopPatternResponseBodyAdvice(): EnvelopPatternResponseBodyAdvice {
+        return EnvelopPatternResponseBodyAdvice()
+    }
+
+    @Bean
+    fun cursorPaginationResponseBodyAdvice(): CursorPaginationAdvice {
+        return CursorPaginationAdvice()
+    }
 }
